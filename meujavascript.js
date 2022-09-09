ed7be1b9ae71fde420d2c71df52fea28adbb9736
Vinicius Almeida dos Santos <102772737+ViniAlmeidaUSJT@users.noreply.github.com>
2022-09-09 15:30:31 -0300

    Add files via upload
    Calculadora Simples

--- meujavascript.js
+++ meujavascript.js
@@ -1,44 +1,31 @@
-/*
-v1 = [];
-v1[0] = 3.4;
-v1[1] = 2;
-v1[2] = "abc";
+//alert("Seja bem-vindo (a)");*\
 
-console.log(v1.length);
+function calculadora(){
 
-v2 = [2, "abc", true];
-console.log(v2);
+    var valor1 = parseInt(document.getElementById("ivalor1").value);
+    var valor2 = parseInt(document.getElementById("ivalor2").value);
 
-for(let i = 0; i < v2.length; i++){
-    console.log(v2[i]);
-}
+    var op = document.querySelector('input[name=calc]:checked').value;
 
-const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"];
-const apenasComA = nomes.filter((n) => n.startsWith("A"));
-console.log(apenasComA);
+    if (op == "soma"){
+        var resposta = valor1 + valor2;
+        alert("A soma é: " + resposta);
+    }
 
-const valores = [1, 2, 3, 4];
-const soma = valores.reduce((ac,v) => ac + v);
-console.log(soma);
-*/
+    if (op == "subtracao"){
+        var resposta = valor1 - valor2;
+        alert("A Subtração é: " + resposta);
+    }
 
-function hello (nome){
-    console.log("Oii.. " + nome);
-}
+    if (op == "multiplicacao"){
+        var resposta = valor1 * valor2;
+        alert("A multiplicação é: " + resposta);
+    }
 
-function soma(a, b){
-    return a + b;
-}
-
-const res = soma(2,3);
-console.log(res);
-
-function ehPar(numero){
-    if (numero%2 == 0){
-        console.log("É par");
-    }else{
-        console.log("É impar");
+    if (op == "divisao"){
+        var resposta = valor1 / valor2;
+        alert("A divisão é: " + resposta);
     }
 }
 
-ehPar(5);
+
